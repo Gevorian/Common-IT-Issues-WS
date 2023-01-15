ac4cc22ca09512addb19bcc4af4570239d450d93
Gevorian <gevorian@gmx.us>
2023-01-15 09:43:00 -0600

    Update Scrape.py

--- Scrape.py
+++ Scrape.py
@@ -20,21 +20,31 @@ def categorize_article(article):
             return category
     return 'Other'
 
+# counter for number of requests
+request_counter = 0
+
 # loop through the websites
 for website in websites:
-    # make a request to the website
-    page = requests.get(website)
-    # parse the HTML content
-    soup = BeautifulSoup(page.content, 'html.parser')
-    # find all the articles on the website
-    articles = soup.find_all('a')
-    # loop through the articles
-    for article in articles:
-        article_text = article.text
-        # categorize the article
-        category = categorize_article(article_text)
-        # add the article to the appropriate category in the results dictionary
-        results[category].append(article_text)
+    if request_counter > 500:
+        print("Reached the request limit of 500 per page.")
+        break
+    try:
+        # make a request to the website
+        page = requests.get(website)
+        request_counter += 1
+        # parse the HTML content
+        soup = BeautifulSoup(page.content, 'html.parser')
+        # find all the articles on the website
+        articles = soup.find_all('a')
+        # loop through the articles
+        for article in articles:
+            article_text = article.text
+            # categorize the article
+            category = categorize_article(article_text)
+            # add the article to the appropriate category in the results dictionary
+            results[category].append(article_text)
+    except requests.exceptions.RequestException as e:
+        print("An error occurred:", e)
 
 # write the results to a CSV file
 with open('it_issues.csv', 'w', newline='', encoding='utf-8') as csvfile:
