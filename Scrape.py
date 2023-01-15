aa559e269caf39f47aa322ab05e7641ebdacfe12
Gevorian <gevorian@gmx.us>
2023-01-15 09:40:12 -0600

    Create Scrape.py

---
+++ Scrape.py
@@ -0,0 +1,46 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+# list of IT news websites to scrape
+websites = ['https://www.cio.com/category/technology/', 'https://www.techradar.com/news/', 'https://www.zdnet.com/topic/tech/']
+
+# categories of IT-related issues
+categories = ['Security', 'Artificial Intelligence', 'Cloud Computing', 'Big Data', 'IoT']
+
+# dictionary to store the results
+results = {}
+for category in categories:
+    results[category] = []
+
+# function to categorize an article based on its content
+def categorize_article(article):
+    for category in categories:
+        if category.lower() in article.lower():
+            return category
+    return 'Other'
+
+# loop through the websites
+for website in websites:
+    # make a request to the website
+    page = requests.get(website)
+    # parse the HTML content
+    soup = BeautifulSoup(page.content, 'html.parser')
+    # find all the articles on the website
+    articles = soup.find_all('a')
+    # loop through the articles
+    for article in articles:
+        article_text = article.text
+        # categorize the article
+        category = categorize_article(article_text)
+        # add the article to the appropriate category in the results dictionary
+        results[category].append(article_text)
+
+# write the results to a CSV file
+with open('it_issues.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    # write the header row
+    writer.writerow(categories)
+    # write the results
+    for category in categories:
+        writer.writerow(results[category])
